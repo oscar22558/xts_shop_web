b0392f9bf2a354937feb1c3362cd46ce460ddd17
Oscar Shum <oscar22558oscar@gmail.com>
2022-09-18 22:26:26 +0800

    corrected typo

--- run-react.sh
+++ run-react.sh
@@ -2,7 +2,7 @@
 source $HOME/.profile
 if [ -z $XTS_APP_ROOT ]
 then 
-    echo "app root directory evn variable is not defined."
+    echo "app root directory env variable is not defined."
     exit 0
 fi
 cd $XTS_APP_ROOT/frontend
