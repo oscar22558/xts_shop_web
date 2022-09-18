33c63c1435e3b8baef1ef8ecd565a51f4bd64828
Oscar Shum <oscar22558oscar@gmail.com>
2022-09-18 20:56:26 +0800

    configured react build env

--- frontend/src/features/ApiConfig.ts
+++ frontend/src/features/ApiConfig.ts
@@ -2,10 +2,10 @@ export const protocol = "http"
 export const domain = "localhost"
 export const port = "8080"
 export const routePrefix = "api"
-export const host = `${protocol}://${domain}`
+export const host = process.env.NODE_ENV === "development" ? `${protocol}://${domain}:${port}` : ""
+export const baseURL = `${host}/${routePrefix}`
 const config = {
-    // baseURL: `${protocol}://${domain}/${routePrefix}`,
-    baseURL: `/${routePrefix}`,
+    baseURL,
     method: "get",
     headers: {
         "Accept": "application/json",
