4bd84ecb731ccef4147d71fb13ef7ca8fbd1d504
Oscar Shum <oscar22558oscar@gmail.com>
2022-09-19 01:21:19 +0800

    update shopping cart page entry button design

--- frontend/src/components/TopBar/components/ShoppingCartIcon.tsx
+++ frontend/src/components/TopBar/components/ShoppingCartIcon.tsx
@@ -1,11 +1,32 @@
 import useCart from "../../../data-sources/cart/useCart"
 import AppRouteList from "../../../routes/AppRouteList"
-import { Link } from "@mui/material"
+import { Badge, createTheme, IconButton, Link } from "@mui/material"
+import MaterialCartIcon from '@mui/icons-material/ShoppingCart';
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#fff',
+            main: '#fff',
+            dark: '#fff',
+            contrastText: '#000',
+        }
+    },
+});
 
 const ShoppingCartIcon = ()=>{
     const {itemTotalCountInCart: itemCountInCart} = useCart()
     return <>
-        <Link href={AppRouteList.cart} color="#fff" underline="none">Shopping Cart ({itemCountInCart()})</Link>
+        <Link href={AppRouteList.cart} color="#fff" underline="none">
+            <ThemeProvider theme={theme} >
+                <IconButton color="primary">
+                    <Badge badgeContent={itemCountInCart()} color="secondary">
+                        <MaterialCartIcon />
+                    </Badge>
+                </IconButton>
+            </ThemeProvider>
+        </Link>
     </>
 }
 export default ShoppingCartIcon
