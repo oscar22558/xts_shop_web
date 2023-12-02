9d01a172f757dd7ee6a55fbdfd1786660200b5b4
oscar22558 <32055343+oscar22558@users.noreply.github.com>
2023-12-02 18:50:18 +0800

    Update StripePromise.ts
    remove secret key from frontend app

--- frontend/src/pages/CheckoutPages/PaymentPage/StripePromise.ts
+++ frontend/src/pages/CheckoutPages/PaymentPage/StripePromise.ts
@@ -1,4 +1,4 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe("pk_test_51LS0ODIlTFFvV5CYLojONvwgp65Y0XGQ5FnXcJHdp4dJ0npvi3bmUebYlPqfv2HZwzWueAIoKdxgpqIRDY5ufQg600dws9t0jV");
+const stripePromise = loadStripe("");
 export default stripePromise
